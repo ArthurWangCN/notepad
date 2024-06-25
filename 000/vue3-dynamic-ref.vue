e4d5be60a7efe3185e1a96a15e23284b62432b98
ArthurWang77 <11898495@qq.com>
2024-06-25 10:45:23 +0800

    Create vue3-dynamic-ref.vue

---
+++ 000/vue3-dynamic-ref.vue
@@ -0,0 +1,32 @@
+# vue3 设置动态 ref 并获取
+
+```vue
+<template>
+  <div v-for="item in list" :ref="(el: refItem) => setRefMap(el, item)" :key="item.name"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import type { ComponentPublicInstance } from 'vue';
+
+type refItem = Element | ComponentPublicInstance | null
+
+const list = [
+  { name: 'ref1'},
+  { name: 'ref2'},
+  { name: 'ref3'},
+  { name: 'ref4'},
+]
+
+const refMap: Record<string, refItem> = {}
+const setRefMap = (el: refItem, item: {name: string}) => {
+  if (el) {
+    refMap[`${item.name}`] = el
+  }
+}
+
+onMounted(() => {
+  console.log(refMap)
+})
+</script>
+```
